b8dd282b6ab0fdc76b12471f9f9f5454fdde4fc8
wlee261 <38049741+wlee261@users.noreply.github.com>
2020-05-16 23:49:23 -0700

    Add files via upload

--- minmax.cpp
+++ minmax.cpp
@@ -2,7 +2,6 @@
 #include <stdexcept>
 #include <iostream>
 #include <sstream>
-#include <string>
 #include <math.h>
 #include <PA2_dataset.txt>
 
@@ -12,51 +11,7 @@ class minmax
     int A[1000000];
     int size = 0;
 
-    void main(int argc, char* argv[])
-    {
-        string line;
-        ifstream myfile ("PA2_dataset.txt");
-        if (myfile.is_open())
-        {
-            while ( getline (myfile,line) )
-            {
-                insert(stoi(line));
-            }
-            myfile.close();
-        }
-
-        else cout << "Unable to open file"; 
-        string commands = argv[1];
-        vector<string> result;
-        stringstream s_stream(commands);
-        while(s_stream.good())
-        {
-            string substr;
-            getline(s_stream, substr, ',');
-            result.push_back(substr);
-        }
-
-        for(int i = result.size(); i > 0; i--)
-        {
-            if(result[i].substr(0,5) == "printH")
-                printHeap();
-            else if(result[i].substr(0,5) == "getMi")
-                getMin();
-            else if(result[i].substr(0,5) == "getMa")
-                getMax();
-            else if(result[i].substr(0,5) == "inser")
-            {
-                insert(getInt(result[i]));
-            }
-            else if(result[i].substr(0,8) == "deleteMa")
-                deleteMax();
-            else if(result[i].substr(0,8) == "deleteMi")
-                deleteMin();
-
-        }
-
-    }
-
+    
     //for convenience
     int getParent(int index)
     {
@@ -372,3 +327,48 @@ class minmax
         return value;
     }
 };
+
+void main(int argc, char* argv[])
+{
+    string line;
+    ifstream myfile ("PA2_dataset.txt");
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            insert(stoi(line));
+        }
+        myfile.close();
+    }
+
+    else cout << "Unable to open file"; 
+    string commands = argv[1];
+    vector<string> result;
+    stringstream s_stream(commands);
+    while(s_stream.good())
+    {
+        string substr;
+        getline(s_stream, substr, ',');
+        result.push_back(substr);
+    }
+
+    for(int i = result.size(); i > 0; i--)
+    {
+        if(result[i].substr(0,5) == "printH")
+            printHeap();
+        else if(result[i].substr(0,5) == "getMi")
+            getMin();
+        else if(result[i].substr(0,5) == "getMa")
+            getMax();
+        else if(result[i].substr(0,5) == "inser")
+        {
+            insert(getInt(result[i]));
+        }
+        else if(result[i].substr(0,8) == "deleteMa")
+            deleteMax();
+        else if(result[i].substr(0,8) == "deleteMi")
+            deleteMin();
+
+    }
+
+}
